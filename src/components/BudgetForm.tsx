9246475aa66873c45a3b276d6a7131d426a5a4a0
MarceBq <marcebq2005@gmail.com>
2025-09-16 00:31:50 -0500

    add BudgetForm component template

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
-
 export default function BudgetForm() {
   return (
-    <div>BudgetForm</div>
-  )
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir Presupuesto
+        </label>
+
+        <input
+          id="budget"
+          name="budget"
+          className="w-full bg-white border border-gray-200 p-2"
+          type="number"
+          placeholder="Define tu presupuesto"
+        />
+      </div>
+
+      <input
+        type="submit"
+        value="Definir Presupuesto"
+        className="w-full bg-blue-600 hover:bg-blue-700 cursor-pointer
+        p-2 text-white font-black uppercase"
+      />
+    </form>
+  );
 }
