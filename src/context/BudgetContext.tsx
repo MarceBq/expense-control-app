fef28fbde87d831a19525d3c59e8c18abee517fc
MarceBq <marcebq2005@gmail.com>
2025-09-16 23:05:31 -0500

    add context to main file

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,23 +1,23 @@
-import { useReducer, createContext } from "react"
-import { budgetReducer, initialState } from "../reducers/budget-reducer"
-import type { BudgetContextProps, BudgetProviderProps } from "../types/context-type"
+import { useReducer, createContext } from "react";
+import { budgetReducer, initialState } from "../reducers/budget-reducer";
+import type {
+  BudgetContextProps,
+  BudgetProviderProps,
+} from "../types/context-type";
 
-export const BudgetContext = createContext<BudgetContextProps>(null!)
+export const BudgetContext = createContext<BudgetContextProps>(null!);
 
-export const BudgetProvider = ({children} : BudgetProviderProps) => {
-    
-    const [state, dispatch] = useReducer(budgetReducer, initialState)
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialState);
 
-
-
-    return (
-        <BudgetContext.Provider 
-            value={{
-                state, 
-                dispatch
-            }}
-        >
-            {children}
-        </BudgetContext.Provider>
-    )
-}
+  return (
+    <BudgetContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </BudgetContext.Provider>
+  );
+};
